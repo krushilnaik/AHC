96920d2835ff6c93cfe70860bcdab715c8b52483
Krushil Naik <krushilnaik96@gmail.com>
2021-07-31 11:36:43 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+import './scss/App.scss';
+
+interface Props {}
+
+function App({}: Props): ReactElement {
+	return <main></main>;
+}
+
+export default App;
